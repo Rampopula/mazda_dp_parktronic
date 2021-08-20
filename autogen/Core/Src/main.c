637e7923f7bff53498c4e0f9f1e2f1a969c5b2cd
Eduard Chaika <rampopula@gmail.com>
2021-08-20 14:57:59 +0300

    chip: Enable parktronic input switch GPIO
    Signed-off-by: Eduard Chaika <rampopula@gmail.com>

--- autogen/Core/Src/main.c
+++ autogen/Core/Src/main.c
@@ -269,6 +269,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
+  /*Configure GPIO pin : PB14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_14;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
