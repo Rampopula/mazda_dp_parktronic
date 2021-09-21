174b84093f06b3e42eca44bd658c19b027995dec
Eduard Chaika <rampopula@gmail.com>
2021-09-21 18:54:21 +0300

    common: Add reset bit and array size definitions
    Signed-off-by: Eduard Chaika <rampopula@gmail.com>

--- common/common.h
+++ common/common.h
@@ -11,10 +11,13 @@
 #define __MDP_COMMON_H__
 
 #define BIT(x)			(1 << x)
+#define RESET_BIT(byte, bit)	(byte &= ~(BIT(bit)))
 
 #define MAX(x, y)		(((x) > (y)) ? (x) : (y))
 #define MIN(x, y)		(((x) < (y)) ? (x) : (y))
 
 #define IN_RANGE(a, x, y)	((a) >= (x) && (a) <= (y))
+#define ARRAY_SIZE(x)		(sizeof(x) / sizeof((x)[0]))
+
 
 #endif /* __MDP_COMMON_H__ */
