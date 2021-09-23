5e7a33f4b0240e866419c9a387269d7694c93adc
Eduard Chaika <rampopula@gmail.com>
2021-09-23 12:03:18 +0300

    boardinfo: Update indentation
    Signed-off-by: Eduard Chaika <rampopula@gmail.com>

--- boardinfo/boardinfo.h
+++ boardinfo/boardinfo.h
@@ -13,18 +13,18 @@
 #include "stm32f1xx_hal.h"
 
 #ifndef MDP_APP_DEBUG
-#define MDP_APP_DEBUG 0
+#define MDP_APP_DEBUG		0
 #endif
 
 #define MDP_PTRONIC_F2616
 
-#define MDP_APP_VERSION "v0.1"
-#define MDP_BOARD_REVISION "rev.01"
-#define MDP_BOARD_CHIP_NAME "STM32F103C8T6"
+#define MDP_APP_VERSION		"v0.1"
+#define MDP_BOARD_REVISION	"rev.01"
+#define MDP_BOARD_CHIP_NAME	"STM32F103C8T6"
 
-#define MDP_CLOCK_FREQ_HZ HAL_RCC_GetHCLKFreq()
-#define MDP_CLOCK_FREQ_MHZ (MDP_CLOCK_FREQ_HZ / (uint32_t)1e6)
+#define MDP_CLOCK_FREQ_HZ	HAL_RCC_GetHCLKFreq()
+#define MDP_CLOCK_FREQ_MHZ	(MDP_CLOCK_FREQ_HZ / (uint32_t)1e6)
 
-#define MDP_DEBUG_UART_SPEED 115200
+#define MDP_DEBUG_UART_SPEED	115200
 
 #endif /* __MDP_BOARDINFO_H__ */
